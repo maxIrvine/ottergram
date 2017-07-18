7ebe5247072a3f1d14af68186faf9ee26651e7eb
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-18 11:17:10 -0400

    adds image switch

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
+var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
+var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var THUMBNAIL_URL_SELECTOR = '[data-image-url]';
 
+function switchImage() {
+    var images = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+    var current = document.querySelector(DETAIL_IMAGE_SELECTOR);
+    images.forEach(function (image){
+        image.addEventListener('click', function (event){
+            event.preventDefault();
+            // changing the image to the image click on
+            current.setAttribute('src', image.getAttribute('href'));
+        });
+    });
+}
+
+switchImage();
